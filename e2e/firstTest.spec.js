a4a766fdd95cf4d47c98555b0e5e896a2ae54b3e
spusapati <srikanth.pusapati@trinet.com>
2018-07-20 09:09:24 -0700

    Updated testcases, ignored bin/exponent.app as its too large to upload in github

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,11 +1,12 @@
 const { reloadApp } = require('detox-expo-helpers');
  
 describe('Login Page Tests', () => {
-  beforeEach(async () => {
+  before(async () => {
     await reloadApp();
   });
 
-  it('should have welcome screen', async () => {
+  it('should have welcome Text', async () => {
+    await waitFor(element(by.id('welcome'))).toBeVisible().withTimeout(1000);
     await expect(element(by.id('welcome'))).toBeVisible();
   });
 
